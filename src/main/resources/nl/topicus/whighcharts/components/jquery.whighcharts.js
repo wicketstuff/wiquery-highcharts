2fec74f76b7fe5266d45b69a6d3e2eab53ef6bcd
Hielke Hoeve <hielke.hoeve@gmail.com>
2011-06-21 14:48:25 +0200

    foutje in de js? parameters komen nu wel door.

--- src/main/resources/nl/topicus/whighcharts/components/jquery.whighcharts.js
+++ src/main/resources/nl/topicus/whighcharts/components/jquery.whighcharts.js
@@ -1,21 +1,12 @@
 (function() {
 	wHighChartsSerializeEvent = function(event) {
-		return
-			"name="+this.name+
-			"&visible="+this.visible+
-			"&category="+this.category+
-			"&x="+this.x+
-			"&y="+this.y+
-			"event.type="+event.type+
-			
+		return "event.type="+event.type+
 			"&event.altKey="+event.altKey+
 			"&event.ctrlKey="+event.ctrlKey+
 			"&event.shiftKey="+event.shiftKey+
 			"&event.checked=" + event.checked+
-			
 			"&event.chartX=" + event.chartX+
 			"&event.chartY=" + event.chartY
-		
 			"&event.xAxis[0].value=" + event.xAxis.value+
 			"&event.xAxis[0].min=" + event.xAxis.min+
 			"&event.xAxis[0].max=" + event.xAxis.max+
