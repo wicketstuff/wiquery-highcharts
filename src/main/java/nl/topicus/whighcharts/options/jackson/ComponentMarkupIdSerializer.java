cf7a96f6f4e462a68b053e44a423c47cf62768fd
Hielke Hoeve <hielke.hoeve@gmail.com>
2011-12-16 15:02:33 +0100

    forgot class

---
+++ src/main/java/nl/topicus/whighcharts/options/jackson/ComponentMarkupIdSerializer.java
@@ -0,0 +1,32 @@
+package nl.topicus.whighcharts.options.jackson;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import org.apache.wicket.Component;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.SerializerProvider;
+import org.codehaus.jackson.map.ser.SerializerBase;
+
+public class ComponentMarkupIdSerializer extends SerializerBase<Component>
+{
+	public ComponentMarkupIdSerializer()
+	{
+		super(Component.class);
+	}
+
+	@Override
+	public void serialize(Component value, JsonGenerator jgen, SerializerProvider provider)
+			throws IOException, JsonGenerationException
+	{
+		jgen.writeRawValue(value.getMarkupId());
+	}
+
+	@Override
+	public JsonNode getSchema(SerializerProvider provider, Type typeHint)
+	{
+		return createSchemaNode("string", true);
+	}
+}
